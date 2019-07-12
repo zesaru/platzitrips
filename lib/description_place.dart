e56b37582d083629872567e5f4c61557510e3baf
Cesar Murillo <namiayumis@gmail.com>
2019-07-12 16:31:30 +0900

    fonts c22

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -58,6 +58,7 @@ class DescriptionPlace extends StatelessWidget {
       child:  new Text(
         descriptionPlace,
         style: const TextStyle(
+          fontFamily: "Lato",
           fontSize: 16.0,
           fontWeight: FontWeight.bold,
           color: Color(0xFF56575a)
@@ -75,8 +76,9 @@ class DescriptionPlace extends StatelessWidget {
           ),
 
           child: Text(
-            "Duwili Ella",
+            namePlace,
             style: TextStyle(
+                fontFamily: "Lato",
                 fontSize: 30.0,
                 fontWeight: FontWeight.w900
             ),
