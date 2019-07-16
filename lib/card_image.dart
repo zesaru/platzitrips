77741a904fa9824d68b79042e3b51907293fe873
Cesar Murillo <namiayumis@gmail.com>
2019-07-16 17:36:10 +0900

    Widget Listview

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class CardImage extends StatelessWidget{
+class  CardImage extends StatelessWidget {
 
-  String pathImage= "assets/img/beach.jpeg";
+  String pathImage = "assets/img/beach.jpeg";
 
   CardImage(this.pathImage);
 
@@ -14,26 +14,30 @@ class CardImage extends StatelessWidget{
       height: 350.0,
       width: 250.0,
       margin: EdgeInsets.only(
-        top: 80.0,
-        left:20.0
+          top: 80.0,
+          left: 20.0
+
       ),
 
       decoration: BoxDecoration(
-        image:DecorationImage(
-          fit:BoxFit.cover,
-          image: AssetImage(pathImage)
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        shape: BoxShape.rectangle,
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: Colors.black38,
-            blurRadius: 15.0,
-            offset: Offset(0.0, 7.0)
-          )
-        ]
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(pathImage)
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          shape: BoxShape.rectangle,
+          boxShadow: <BoxShadow>[
+            BoxShadow (
+                color:  Colors.black38,
+                blurRadius: 15.0,
+                offset: Offset(0.0, 7.0)
+            )
+          ]
+
       ),
     );
-    return null;
+
+    return card;
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'description_place.dart';
 import 'review_list.dart';
-import 'gradient_back.dart';
+import 'header_appbar.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
                 ReviewList()
               ],
             ),
-            GradientBack("Popular")
+            HeaderAppBar()
           ],
         ),
       ),
