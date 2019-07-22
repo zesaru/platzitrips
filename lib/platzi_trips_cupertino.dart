1328cb59be5275336a347cbcbe89aa2b28e05756
Cesar Murillo <namiayumis@gmail.com>
2019-07-22 11:17:26 +0900

    add profile_background

--- lib/platzi_trips_cupertino.dart
+++ lib/platzi_trips_cupertino.dart
@@ -11,20 +11,20 @@ class PlatziTripsCupertino extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home, color: Colors.indigo),
-                title: Text("")
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search, color: Colors.indigo),
-                title: Text("")
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.indigo),
-                title: Text("")
-            ),
-          ]
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home, color: Colors.indigo),
+                  title: Text("")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search, color: Colors.indigo),
+                  title: Text("")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person, color: Colors.indigo),
+                  title: Text("")
+              ),
+            ]
         ),
 
         tabBuilder: (BuildContext context, int index) {
@@ -44,9 +44,12 @@ class PlatziTripsCupertino extends StatelessWidget {
                 builder: (BuildContext context) => ProfileTrips(),
               );
               break;
+
           }
+
         },
       ),
     );
   }
+
 }
