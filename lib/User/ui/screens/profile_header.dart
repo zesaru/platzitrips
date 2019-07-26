fb8daa0fbd935a9ef2a2e2dcf98146a0973cfee6
Cesar Murillo <namiayumis@gmail.com>
2019-07-26 16:45:11 +0900

    Monitoreando y validando la conexión al Sign con Google

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzitrips/User/bloc/bloc_user.dart';
 import 'package:platzitrips/User/ui/widgets/user_info.dart';
 import 'package:platzitrips/User/ui/widgets/button_bar.dart';
 
 class ProfileHeader extends StatelessWidget {
+
+  UserBloc userBloc;
+
   @override
   Widget build(BuildContext context) {
 
-    final title = Text(
+    userBloc = BlocProvider.of<UserBloc>(context);
+
+    return StreamBuilder(
+      stream: userBloc.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot ) {
+        // ignore: missing_return
+        switch(snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return null;
+          case ConnectionState.done:
+            return null;
+        }
+      },
+    );
+
+    /*final title = Text(
       'Profile',
       style: TextStyle(
           fontFamily: 'Lato',
@@ -33,7 +57,15 @@ class ProfileHeader extends StatelessWidget {
           ButtonsBar()
         ],
       ),
-    );
+    );*/
   }
 
+  Widget showProfileData(AsyncSnapshot snapshot){
+   if(!snapshot.hasData || snapshot.error) {
+     print("No logueado");
+   }
+   else {
+     print("Logueado");
+    }
+  }
 }
