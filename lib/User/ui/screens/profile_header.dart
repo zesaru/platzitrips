6dec0698eff44b8076c4e7b30b1d8a13eb4069fb
Cesar Murillo <namiayumis@gmail.com>
2019-07-31 15:19:54 +0900

    profile gmail

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzitrips/User/bloc/bloc_user.dart';
+import 'package:platzitrips/User/model/user.dart';
 import 'package:platzitrips/User/ui/widgets/user_info.dart';
 import 'package:platzitrips/User/ui/widgets/button_bar.dart';
 
 class ProfileHeader extends StatelessWidget {
-
   UserBloc userBloc;
+  User user;
 
   @override
   Widget build(BuildContext context) {
-
     userBloc = BlocProvider.of<UserBloc>(context);
 
     return StreamBuilder(
       stream: userBloc.streamFirebase,
-      builder: (BuildContext context, AsyncSnapshot snapshot ) {
-        // ignore: missing_return
-        switch(snapshot.connectionState) {
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        switch(snapshot.connectionState){
           case ConnectionState.waiting:
             return CircularProgressIndicator();
           case ConnectionState.none:
             return CircularProgressIndicator();
           case ConnectionState.active:
-            return null;
+            return showProfileData(snapshot);
           case ConnectionState.done:
-            return null;
+            return showProfileData(snapshot);
         }
+
       },
     );
+
 
     /*final title = Text(
       'Profile',
@@ -39,7 +40,6 @@ class ProfileHeader extends StatelessWidget {
           fontSize: 30.0
       ),
     );
-
     return Container(
       margin: EdgeInsets.only(
           left: 20.0,
@@ -53,19 +53,64 @@ class ProfileHeader extends StatelessWidget {
               title
             ],
           ),
-          UserInfo('assets/img/cesar.jpg', 'Cesar Murillo','zesaru@platzi.com'),
+          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
           ButtonsBar()
         ],
       ),
     );*/
   }
 
-  Widget showProfileData(AsyncSnapshot snapshot){
-   if(!snapshot.hasData || snapshot.error) {
-     print("No logueado");
-   }
-   else {
-     print("Logueado");
+
+  Widget showProfileData(AsyncSnapshot snapshot) {
+    if(!snapshot.hasData ||snapshot.hasError){
+      print("No logeado");
+      return Container(
+        margin: EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text("No se pudo cargar la información. Haz login")
+          ],
+        ),
+      );
+    }else{
+      print("Logeado");
+      print(snapshot.data);
+      user = User(name: snapshot.data.displayName, email: snapshot.data.email, photoURL: snapshot.data.photoUrl);
+      final title = Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0
+        ),
+      );
+
+      return Container(
+        margin: EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                title
+              ],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+
     }
   }
+
 }
