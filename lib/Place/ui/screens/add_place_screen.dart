f706ff1c134f095abc9f61932d56fec91ddcb80e
Cesar Murillo <namiayumis@gmail.com>
2019-08-08 15:22:43 +0900

    Creando una Safe Area para una interfaz que tiene un AppBar

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:platzitrips/widgets/gradient_back.dart';
 import 'package:platzitrips/widgets/title_header.dart';
+import 'package:platzitrips/widgets/text_input.dart';
+
 
 class AddPlaceScreen extends StatefulWidget {
 
@@ -20,11 +22,15 @@ class _AddPlaceScreen extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
           GradientBack(height: 300.0,),
-          Row(
+          Row( //App Bar
             children: <Widget>[
               Container(
                 padding: EdgeInsets.only(top: 25.0, left: 5.0),
@@ -39,13 +45,45 @@ class _AddPlaceScreen extends State<AddPlaceScreen> {
                   ),
                 ),
               ),
+
               Flexible(
-                child: Container(
-                    padding: EdgeInsets.only(top: 45.0 , left: 20.0, right: 10.0),
-                    child: TitleHeader(title: "Add a new Place",)
-                  )
-                )
+                  child: Container(
+                    padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 10.0),
+                    child: TitleHeader(title: "Add a new Place"),
+                  ))
+
+
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom:20.0),
+            child: ListView(
+              children: <Widget>[
+                Container(), //Foto
+                Container(//TextField Title
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: "Title",
+                    inputType: null,
+                    maxLines: 1,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                TextInput(//Description
+                  hintText: "Description",
+                  inputType: TextInputType.multiline,
+                  maxLines: 4,
+                  controller: _controllerDescriptionPlace,
+                ),
+//                Container(
+//                  margin: EdgeInsets.only(top: 20.0),
+//                  child: TextInputLocation(
+//                    hintText: "Add Location",
+//                    iconData: Icons.location_on,),
+//                )
+              ],
+            ),
+
           )
         ],
       ),
