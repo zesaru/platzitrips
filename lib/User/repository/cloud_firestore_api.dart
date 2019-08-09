9f5683a65e2831e692db13dc5e31b12f07b795cd
Cesar Murillo <namiayumis@gmail.com>
2019-08-09 16:52:14 +0900

    Envío de datos de un fórmulario en Flutter

--- lib/User/repository/cloud_firestore_api.dart
+++ lib/User/repository/cloud_firestore_api.dart
@@ -1,23 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:platzitrips/User/model/user.dart';
+import 'package:platzitrips/Place/model/place.dart';
 
 class CloudFirestoreAPI {
 
   final String USERS = "users";
-  final String PLACE = "places";
+  final String PLACES = "places";
 
   final Firestore _db = Firestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void updateUserData (User user) async {
+  void updateUserData(User user) async{
     DocumentReference ref = _db.collection(USERS).document(user.uid);
-    return ref.setData({
+    return await ref.setData({
       'uid': user.uid,
       'name': user.name,
       'email': user.email,
       'photoURL': user.photoURL,
       'myPlaces': user.myPlaces,
       'myFavoritePlaces': user.myFavoritePlaces,
-      'lastSignIn': DateTime.now(),
-    },merge: true);
+      'lastSignIn': DateTime.now()
+
+    }, merge: true);
+
   }
+
+  Future<void> updatePlaceData(Place place) async {
+    CollectionReference refPlaces = _db.collection(PLACES);
+
+    await _auth.currentUser().then((FirebaseUser user){
+      refPlaces.add({
+        'name' : place.name,
+        'description': place.description,
+        'likes': place.likes,
+        'userOwner': "${USERS}/${user.uid}",//reference
+      });
+    });
+
+
+  }
+
 }
